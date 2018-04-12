04468a9ca5400521823cc990f4c49cf5aa4ef834
JonnaPartanen <33378336+JonnaPartanen@users.noreply.github.com>
2018-04-12 10:56:42 +0300

    update get

--- app_server/Controllers/ctrlMovies.js
+++ app_server/Controllers/ctrlMovies.js
@@ -1,15 +1,34 @@
+const request = require('request'); //require the request module and the apiURLs file
+const apiURL = require('./apiURLs');
 
 const list = function(req, res){
-    res.render('movies',{
-        movies:
-        [
-            {movie:'Forrest Gump', director: 'Director: Robert Zemeckis '},
-            {movie:'Everest', director: 'Director: Baltasar Kormákur '},
-            {movie:'Seven', director: ' Director: David Fincher '}
-        ]
 
-    });
+    const path = '/api/movies';
+    const requestOptions = {
+        url : apiURL.server + path,
+        method : 'GET',
+        json : {},
+        qs : {}
+    };
+
+    request(
+        requestOptions,
+        function (err, response, body){
+            if(err){
+                res.render('error', {message: err.message});
+            }else if (response.statusCode !== 200){
+                res.render('error', {message: 'Error accessing API: ' + response.statusMessage + " ("+ response.statusCode + ")" });
+            }else if (!(body instanceof Array)) {
+                res.render('error', {message: 'Unexpected response data'});
+            }else if (!body.length){
+                res.render('error', {message: 'No documents in collection'});
+            }else{
+                res.render('movies', {movies: body});
+            }
+        }
+    );
 };
+
 module.exports = {
     list
 };
--- app_server/Controllers/ctrlSeries.js
+++ app_server/Controllers/ctrlSeries.js
@@ -1,13 +1,31 @@
-const list = function(req, res){
-    res.render('series',{
-        series:
-            [
-                {serie:'Breaking Bad'},
-                {serie:'24'},
-                {serie:'Sons of Anarghy'}
-            ]
+const request = require('request'); //require the request module and the apiURLs file
+const apiURL = require('./apiURLs');
 
-    });
+const list = function(req, res){
+    const path = '/api/series';
+    const requestOptions = {
+        url : apiURL.server + path,
+        method : 'GET',
+        json : {},
+        qs : {}
+    };
+
+    request(
+        requestOptions,
+        function (err, response, body){
+            if(err){
+                res.render('error', {message: err.message});
+            }else if (response.statusCode !== 200){
+                res.render('error', {message: 'Error accessing API: ' + response.statusMessage + " ("+ response.statusCode + ")" });
+            }else if (!(body instanceof Array)) {
+                res.render('error', {message: 'Unexpected response data'});
+            }else if (!body.length){
+                res.render('error', {message: 'No documents in collection'});
+            }else{
+                res.render('series', {series: body});
+            }
+        }
+    );
 };
 module.exports = {
     list
